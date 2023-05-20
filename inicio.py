5836c80c3e85e8f9451cfc0a5a8f707cd47cd7c8
Lucas Santos <lucass4@al.insper.edu.br>
2023-05-20 20:56:03 -0300

    modified:   commit 	new file:   inicio.py

---
+++ inicio.py
@@ -0,0 +1,12 @@
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+import random
+
+pygame.init()
+
+# ----- Gera tela principal
+LARGURA = 480
+ALTURA = 600
+
+janela = pygame.display.set_mode((LARGURA, ALTURA))
