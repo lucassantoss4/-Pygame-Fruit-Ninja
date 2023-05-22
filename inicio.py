ee961be4a570648095c1d383a6fcd3e6c546c1e9
Gabrielly <gabriellycs@al.insper.edu.br>
2023-05-22 16:29:57 -0300

    gabi

--- inicio.py
+++ inicio.py
@@ -21,8 +21,6 @@ janela = pygame.display.set_mode((LARGURA, ALTURA))
 
 backgroung = pygame.image.load('util/img/background.png').convert() #A- denominei o fundo como background
 backgroung = pygame.transform.scale(backgroung, (LARGURA, ALTURA)) #A- escala
-
-#######gabi começou a mexer aqui
 pygame.display.set_caption('Fruit Ninja')
 
 # ----- Inicia estruturas de dados
@@ -37,7 +35,7 @@ while game:
             game = False
         if event.type == pygame.KEYUP:
             game = False
-
+            
     # ----- Gera saídas
     janela.fill(PRETO)  # Preenche com a cor preta
     janela.blit(backgroung, (0,0)) #A - coloquei o fundo na janela
@@ -56,13 +54,63 @@ class Logos(pygame.sprite.Sprite):
         
         self.image = img
         self.rect = self.image.get_rect()
-        self.rect.x = random.randint(50, LARGURA-50) #as 'frutinhas' são lançadas de baixo pracima, e não são lançadas no canto
+        self.rect.x = random.randint(50, LARGURA-50) #as 'frutinhas' não são lançadas no canto
         self.rect.y = 600 #as 'frutinhas' são lançadas de baixo
+    
+        #quantidade de frutas lançadas
+        self.qtd = random.randint(0, 6)
         
         #a velocidade tem que mudar de sentido em algum momento 
         #no eixo x a velocidade inicial pode ser positiva ou negativa, mas não quero velocidades tõ diferentes 
         self.speedx = random.randint(-2, 2)
         #no eixo y eu quero que as velocidades não sejam tão diferentes
-        self.speedy = random.randint(4, 6)
+        self.speedy = random.randint(-4, -6)
         
-######gabi mexeu até aqui
+        def update(self):
+            #vai atualizar as posições das frutas    
+            self.rect.x += self.speedx
+            self.rect.y += self.speedy
+            
+        
+#usar a mesma lógica para construir a série de bombas
+class Bombas(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(50, LARGURA-50) #as bombas não são lançadas no canto
+        self.rect.y = 600 #as bombas são lançadas de baixo
+        
+        #vão ser lançadas 0, 1 ou duas bombas cada vez
+        self.qtd = random.randint(0, 2)
+        
+        #a velocidade tem que mudar de sentido em algum momento 
+        #no eixo x a velocidade inicial pode ser positiva ou negativa, mas não quero velocidades tõ diferentes 
+        self.speedx = random.randint(-2, 2)
+        #no eixo y eu quero que as velocidades não sejam tão diferentes
+        self.speedy = random.randint(-4, -6)
+        
+        
+
+
+
+### problemas/coias que a gabi pensou
+
+        """
+        
+Falta fazer o update de cada classe
+não sei como imitar o efeito de queda livre (fazer a fruta desacelerar)
+
+eu quero que as frutas sejam lançadas de novo 
+somente quando todas as outras frutas (lançadas anteriormente) 
+tiverem saído da tela      
+
+as frutas não precisam ser lançadas todas ao mesmo tempo, pode ter um 
+intervalo de lançameto entre elas, teria como fazer isso?  
+
+
+
+
+        """
