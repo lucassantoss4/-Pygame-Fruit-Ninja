7b62a8331094b9b047df61410a8c9b1480db0309
Gabrielly <gabriellycs@al.insper.edu.br>
2023-05-22 17:09:02 -0300

    classes - gabi

--- Classes.py
+++ Classes.py
@@ -1,3 +1,7 @@
+import pygame
+import random
+from geral import PRETO, BRANCO, VERMELHO, AMARELO, VERDE, AZUL, ROXO, LARGURA, ALTURA
+
 #criando a classe de logos
 class Logos(pygame.sprite.Sprite):
     def __init__(self, img):
@@ -8,20 +12,20 @@ class Logos(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = random.randint(50, LARGURA-50) #as 'frutinhas' não são lançadas no canto
         self.rect.y = 600 #as 'frutinhas' são lançadas de baixo
-    
-        #quantidade de frutas lançadas
-        self.qtd = random.randint(0, 6)
         
         #a velocidade tem que mudar de sentido em algum momento 
         #no eixo x a velocidade inicial pode ser positiva ou negativa, mas não quero velocidades tõ diferentes 
-        self.speedx = random.randint(-2, 2)
+        self.speedx = random.randint(-1, 1)
         #no eixo y eu quero que as velocidades não sejam tão diferentes
-        self.speedy = random.randint(-4, -6)
+        self.speedy = random.randint(-3, -5)
         
-        def update(self):
-            #vai atualizar as posições das frutas    
-            self.rect.x += self.speedx
-            self.rect.y += self.speedy
+    def update(self):
+        #vai atualizar a velocidade
+        self.speedy += 0.2
+                
+        #vai atualizar a posição
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
             
         
 #usar a mesma lógica para construir a série de bombas
@@ -34,23 +38,34 @@ class Bombas(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = random.randint(50, LARGURA-50) #as bombas não são lançadas no canto
         self.rect.y = 600 #as bombas são lançadas de baixo
-        
-        #vão ser lançadas 0, 1 ou duas bombas cada vez
-        self.qtd = random.randint(0, 2)
-        
+                
         #a velocidade tem que mudar de sentido em algum momento 
         #no eixo x a velocidade inicial pode ser positiva ou negativa, mas não quero velocidades tõ diferentes 
-        self.speedx = random.randint(-2, 2)
+        self.speedx = random.randint(-1, 1)
         #no eixo y eu quero que as velocidades não sejam tão diferentes
-        self.speedy = random.randint(-4, -6)
+        self.speedy = random.randint(-3, -5)
         
+    def update(self):
+        #vai atualizar a velocidade
+        self.speedy += 0.2
+                
+        #vai atualizar a posição
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
         
+    
+ 
+#quantidade de frutas lançadas
+n_frutas = random.randint(0, 6)
 
+#quantidade de bombas lançadas
+n_bombas = random.randint(0, 2)      
+    
 
 
 ### problemas/coias que a gabi pensou
 
-        """
+"""
         
 Falta fazer o update de cada classe
 não sei como imitar o efeito de queda livre (fazer a fruta desacelerar)
@@ -62,7 +77,6 @@ tiverem saído da tela
 as frutas não precisam ser lançadas todas ao mesmo tempo, pode ter um 
 intervalo de lançameto entre elas, teria como fazer isso?  
 
+Mudar o cursor do mouse pra uma faca
 
-
-
-        """
+"""
