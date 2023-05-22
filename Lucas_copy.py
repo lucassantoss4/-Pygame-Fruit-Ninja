1ee41e05e7571a889befbb5caf1e6f48ec87c71f
Lucas Santos <lucass4@al.insper.edu.br>
2023-05-22 17:28:14 -0300

    modified:   Lucas_copy.py

---
+++ Lucas_copy.py
@@ -0,0 +1,56 @@
+# ===== Inicialização =====
+
+'''
+Legendas de atualização <3
+A <3
+G <3
+L <3
+'''
+
+# ----- Importa e inicia pacotes
+import pygame
+import random
+from geral import PRETO, BRANCO, VERMELHO, AMARELO, VERDE, AZUL, ROXO, LARGURA, ALTURA
+    #A- criei o arquivo geral.py pra guardar coisas que podemos usar frequentemente
+
+
+pygame.init()
+
+# pontuação
+pontuação = 0
+
+
+# ----- Gera tela principal
+
+# configurações da tela e fonte do jogo
+janela = pygame.display.set_mode((LARGURA, ALTURA))
+janela.fill(BRANCO)
+fonte = pygame.font.SysFont('Comic Sans MS', 30)
+texto = fonte.render('Fruit Ninja', True, PRETO, BRANCO)
+backgroung = pygame.image.load('util/img/background.png').convert() #A- denominei o fundo como background
+backgroung = pygame.transform.scale(backgroung, (LARGURA, ALTURA)) #A- escala
+pygame.display.set_caption('Fruit Ninja')
+
+# ----- Inicia estruturas de dados
+game = True
+
+# ===== Loop principal =====
+while game:
+    
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+        if event.type == pygame.KEYUP:
+            game = False
+            
+    # ----- Gera saídas
+    janela.fill(PRETO)  # Preenche com a cor preta
+    janela.blit(backgroung, (0,0)) #A - coloquei o fundo na janela
+
+    # ----- Atualiza estado do jogo
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
