f6eb79a058d47114b969ef15eeecb3669cd05725
alis-on-araujo <alisonao@al.insper.edu.br>
2023-05-24 15:15:58 -0300

    sons

--- geral.py
+++ geral.py
@@ -49,5 +49,44 @@ mec_img = pygame.transform.scale(mec_img, (ALTURA_OBJ, LARGURA_OBJ))
 mecat_img = pygame.image.load('util/img/MECAT.png').convert_alpha()
 mecat_img = pygame.transform.scale(mecat_img, (ALTURA_OBJ, LARGURA_OBJ))
 
-#lista com as logos dos cursos
+
+
+###SONS
+
+#hits
+hit_0 = pygame.mixer.music.load('util/hits/hit0.mp3')
+hit_0 = pygame.mixer.music.set_volume(0.4)
+
+hit_1 = pygame.mixer.music.load('util/hits/hit_1.mp3')
+hit_1 = pygame.mixer.music.set_volume(0.4)
+
+hit_2 = pygame.mixer.music.load('util/hits/hit2.mp3')
+hit_2 = pygame.mixer.music.set_volume(0.4)
+
+hit_3 = pygame.mixer.music.load('util/hits/hit3.mp3')
+hit_3 = pygame.mixer.music.set_volume(0.4)
+
+hit_4 = pygame.mixer.music.load('util/hits/hit4.mp3')
+hit_4 = pygame.mixer.music.set_volume(0.4)
+
+hit_5 = pygame.mixer.music.load('util/hits/hit5.mp3')
+hit_5 = pygame.mixer.music.set_volume(0.4)
+
+hit_6 = pygame.mixer.music.load('util/hits/hit6.mp3')
+hit_6 = pygame.mixer.music.set_volume(0.4)
+
+hit_7 = pygame.mixer.music.load('util/hits/hit7.mp3')
+hit_7 = pygame.mixer.music.set_volume(0.4)
+
+hit_8 = pygame.mixer.music.load('util/hits/hit8.mp3')
+hit_8 = pygame.mixer.music.set_volume(0.4)
+
+hit_9 = pygame.mixer.music.load('util/hits/hit9.mp3')
+hit_9 = pygame.mixer.music.set_volume(0.4)
+
+hit_10 = pygame.mixer.music.load('util/hits/hit10.mp3')
+hit_10 = pygame.mixer.music.set_volume(0.4)
+
+#listas
 lista_logos = [adm_img, ccomp_img, direito_img, ecomp_img, econo_img, faca_img, mec_img, mecat_img]
+lista_hits = [hit_0, hit_1, hit_2, hit_3, hit_4, hit_5, hit_6, hit_7, hit_8, hit_9, hit_10]
