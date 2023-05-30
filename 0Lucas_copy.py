816d7ad6fe5ebd5b855d26a14609aa95381d7078
Lucas Santos <lucass4@al.insper.edu.br>
2023-05-30 17:23:45 -0300

    modified:   0Lucas_copy.py

--- 0Lucas_copy.py
+++ 0Lucas_copy.py
@@ -1,12 +1,4 @@
 # ===== Inicialização =====
-
-'''
-Legendas de atualização <3
-A <3
-G <3
-L <3
-'''
-
 # ----- Importa e inicia pacotes
 import pygame
 import random
@@ -153,6 +145,60 @@ class Bombas(pygame.sprite.Sprite):
         self.rect.y += self.speedy
 
 
+# função para exibir a tela de game over
+def Tela_Game_Over(LARGURA, ALTURA):
+    Tela_Game_Final = True # variável para o loop da tela de game over
+    tempo_maximo = 1  # tempo máximo para a tela de game over
+    tempo_inicial = time.time()  # tempo inicial do jogo
+
+    while Tela_Game_Final:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen = pygame.display.set_mode((LARGURA, ALTURA))  # cria a tela
+        pygame.display.set_caption('Game Over')  # nome da tela
+        font = pygame.font.Font("util/fonte/upheavtt.ttf", 60)
+        text = font.render('Game Over', True, (200, 0, 0))  # cor do texto e escrita
+        text_rect = text.get_rect(center=(LARGURA // 2, ALTURA // 2 - 10))  # posição do texto
+
+        # ----- Gera saídas
+
+        screen.fill((0, 0, 0))  # Preenche com a cor preta no fundo
+        screen.blit(text, text_rect)  # coloca o texto na tela
+        pygame.display.flip()  # atualiza a tela
+
+        # Verificar se o tempo máximo foi atingido
+        tempo_atual = time.time()
+        tempo_decorrido = tempo_atual - tempo_inicial  # tempo decorrido desde o início do jogo
+        if tempo_decorrido >= tempo_maximo:
+            Tela_Game_Final = False
+
+# função para exibir a tela de game over
+def Tela_Iniciar_Botao(janela, fundo_pixel, mouser_img):
+    botao_inicia = True
+
+    while botao_inicia:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                botao_inicia = False
+
+        mouser_img_pos = pygame.mouse.get_pos()  # obtem a posição do mouse para desenhar a imagem do mouse
+
+        # ----- Gera saídas
+        janela.blit(fundo_pixel, (0, 0))  # coloca a imagem de fundo na tela
+
+        # Desenhe o cursor do mouse
+        janela.blit(mouser_img, mouser_img_pos)  # coloca a imagem do mouse na tela
+
+        pygame.display.flip()  # Mostra o novo frame para o jogador
+
+
 ##A- Cria grupos
 todas_bombas = pygame.sprite.Group()
 todas_logos = pygame.sprite.Group()
@@ -176,7 +222,7 @@ for i in range(n_logos):
 pygame.mouse.set_visible(False) #tira o mouse da tela
 # faca_img_rect = faca_img.get_rect()
 machado_img_rect = machado_img.get_rect() 
-mouser_img_rect = mouser_img.get_rect() #cria um retângulo para o mouse
+# mouser_img_rect = mouser_img.get_rect() #cria um retângulo para o mouse
 
 
 #------------ Jogo -----------#
@@ -189,163 +235,94 @@ FPS = 60
 Score = 0
 vida = 3 
 
+estado = "JOGANDO"
+
 
 ######### Tela de início #########
-botao_inicia = True
-while botao_inicia:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            botao_inicia = False
-
-    mouser_img_pos = pygame.mouse.get_pos() #obtem a posição do mouse para desenhar a imagem do mouse
-
-    # ----- Gera saídas
-    # janela.fill((0, 0, 0))  # Preenche com a cor preta no fundo
-    janela.blit(fundo_pixel , (0, 0)) #coloca a imagem de fundo na tela
-
-    # Desenhe o cursor do mouse
-    # pygame.draw.circle(janela, VERMELHO, mouser_img, 10) #desenha um círculo vermelho no mouse
-    # Desenha a imagem do cursor personalizado na posição do mouse
-    
-    # pygame.draw.polygon
-    janela.blit(mouser_img, mouser_img_pos) #coloca a imagem do mouse na tela
-
-    pygame.display.flip()  # Mostra o novo frame para o jogador
+Tela_Iniciar_Botao(janela, fundo_pixel, mouser_img) # chama a função para exibir a tela de início
 
 game = True
 
-# ===== Loop principal =====
-pygame.mixer.music.play(loops=-1) #para tocar a música de fundo em loop
-while game:
-    #definindo tempo para execução do loop
-    clock.tick(FPS)
+pygame.mixer.music.play(loops=-1)  # Inicia a reprodução da música de fundo em loop
+while estado == "JOGANDO" :
+    clock.tick(FPS)  # Limita o FPS do jogo
     
-    # ----- Trata eventos
-    for event in pygame.event.get():
-        # ----- Verifica consequências
-        if event.type == pygame.QUIT:
-            game = False
+    for event in pygame.event.get():  # Obtém todos os eventos do Pygame
+        if event.type == pygame.QUIT:  # Verifica se o evento é o de fechar a janela
+            game = False  # Encerra o jogo
             
-    if vida == 0:
-        game = False
+    if vida == 0:  # Verifica se a vida do jogador é igual a zero
+        # game = False  # Encerra o jogo
+        estado = "PERDEU"  # Muda o estado do jogo para 
 
-    #obtem a posição do mouse para cortar as frutas  
-    mouse_x, mouse_y = pygame.mouse.get_pos()
+    mouse_x, mouse_y = pygame.mouse.get_pos()  # Obtém a posição do mouse
     
-    
-    
-    #--------- trata das logos e bombas
-    for logo in todas_logos:
+    for logo in todas_logos:  # Percorre todos os objetos do grupo "todas_logos"
         if logo.rect.x < mouse_x < (logo.rect.x + ALTURA_OBJ) and logo.rect.y < mouse_y < (logo.rect.y + LARGURA_OBJ):
-            logo.kill()
-            Score += 100
-
+            # Verifica se a posição do mouse está dentro das coordenadas do objeto "logo"
+            logo.kill()  # Remove o objeto do grupo
+            Score += 100  # Incrementa a pontuação do jogador
 
         if logo.rect.x > LARGURA or logo.rect.x - ALTURA_OBJ < 0:
-            logo.kill()
+            # Verifica se o objeto "logo" está fora dos limites da tela horizontalmente
+            logo.kill()  # Remove o objeto do grupo
         if logo.rect.y > ALTURA:
-            logo.kill()
-    
-    for bomba in todas_bombas:
+            # Verifica se o objeto "logo" está fora dos limites da tela verticalmente
+            logo.kill()  # Remove o objeto do grupo
+
+    for bomba in todas_bombas:  # Percorre todos os objetos do grupo "todas_bombas"
         if bomba.rect.x < mouse_x < (bomba.rect.x + ALTURA_OBJ) and bomba.rect.y < mouse_y < (bomba.rect.y + LARGURA_OBJ):
-            bomba.kill()
-            vida -= 1
+            # Verifica se a posição do mouse está dentro das coordenadas do objeto "bomba"
+            bomba.kill()  # Remove o objeto do grupo
+            vida -= 1  # Decrementa a vida do jogador
         if bomba.rect.x > LARGURA or bomba.rect.x - ALTURA_OBJ < 0:
-            bomba.kill()
+            # Verifica se o objeto "bomba" está fora dos limites da tela horizontalmente
+            bomba.kill()  # Remove o objeto do grupo
         if bomba.rect.y > ALTURA:
-            bomba.kill()
+            # Verifica se o objeto "bomba" está fora dos limites da tela verticalmente
+            bomba.kill()  # Remove o objeto do grupo
             
-        
     if len(todas_bombas) == 0 and len(todas_logos) == 0:
-        #adiciona elementos nos grupos
-            #sorteia a quantidade de elementos de cada
-        n_logos = random.randint(0, 6)
-        n_bombas = random.randint(0, 2)  
+        # Verifica se não existem mais objetos nos grupos "todas_bombas" e "todas_logos"
+        n_logos = random.randint(0, 6)  # Gera um número aleatório de logos
+        n_bombas = random.randint(0, 2)  # Gera um número aleatório de bombas
         
         for i in range(n_bombas):
+            # Cria e adiciona objetos do tipo "bomba" ao grupo "todas_bombas"
             bomba = Bombas(bomba_img)
             todas_bombas.add(bomba)
 
         for i in range(n_logos):
-            imagem = random.randint(0, 6)
+            # Cria e adiciona objetos do tipo "fruta" ao grupo "todas_logos"
+            imagem = random.randint(0, 6)  # Escolhe uma imagem aleatória para a fruta
             fruta = Logos(lista_logos[imagem])
             todas_logos.add(fruta)        
 
-        
-    # ----- Atualiza estado do jogo
-   
-    todas_bombas.update()
-    todas_logos.update()        
+    todas_bombas.update()  # Atualiza a posição e o estado dos objetos do grupo "todas_bombas"
+    todas_logos.update()  # Atualiza a posição e o estado dos objetos do grupo "todas_logos"        
             
-    # ----- Gera saídas
+    fonte_score = pygame.font.Font("util/fonte/upheavtt.ttf", 35)  # Cria uma fonte para o texto
+    texto_score = fonte_score.render('Score {0}'.format(Score), True, PRETO)  # Renderiza o texto da pontuação
+    texto_vidas = fonte_score.render('Vidas {0}'.format(vida), True, PRETO)  # Renderiza o texto das vidas
     
-    fonte_score = pygame.font.Font("util/fonte/upheavtt.ttf", 35) #cria uma fonte
-    texto_score = fonte_score.render('Score {0}'.format(Score), True, PRETO) #cria um texto com a fonte criada
-    texto_vidas = fonte_score.render('Vidas {0}'.format(vida), True, PRETO) #cria um texto com a fonte criada
-    
-    janela.blit(background, (0,0)) # fundo na janela
-    janela.blit(texto_score, (100, 5))# texto da pontuação na janela
-    janela.blit(texto_vidas, (700,5)) # texto das vidas na janela
-    # faca_img_rect.center = pygame.mouse.get_pos()
-    machado_img_rect.center = pygame.mouse.get_pos()  # coloquei o machado na posição do mouse
-    # janela.blit(faca_img, faca_img_rect) 
-    janela.blit(machado_img, machado_img_rect) # coloca o machado na janela na posição do mouse
-    #------- Desenha bombas e logos
-    todas_bombas.draw(janela)
-    todas_logos.draw(janela)
-    
-    pygame.display.update()  # Mostra o novo frame para o jogador
+    janela.blit(background, (0,0))  # Desenha o fundo na janela
+    janela.blit(texto_score, (100, 5))  # Desenha o texto da pontuação na janela
+    janela.blit(texto_vidas, (700,5))  # Desenha o texto das vidas na janela
+    machado_img_rect.center = pygame.mouse.get_pos()  # Posiciona o machado na posição do mouse
+    janela.blit(machado_img, machado_img_rect)  # Desenha o machado na janela na posição do mouse
 
-# se o jogador apertar o botão de fechar a tela, o jogo fecha
-if event.type != pygame.QUIT:    
-    Tela_Game_Final = True
-    tempo_maximo = 1  # tempo máximo para a tela de game over
-    tempo_inicial = time.time()  # tempo inicial do jogo
-    while Tela_Game_Final:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                # sys.exit()
+    todas_bombas.draw(janela)  # Desenha todos os objetos do grupo "todas_bombas" na janela
+    todas_logos.draw(janela)  # Desenha todos os objetos do grupo "todas_logos" na janela
 
-        screen = pygame.display.set_mode((LARGURA, ALTURA))  # cria a tela
-        pygame.display.set_caption('Game Over')  # nome da tela
-        font = pygame.font.Font("util/fonte/upheavtt.ttf", 60)
-        text = font.render('Game Over', True, (200, 0, 0))  # cor do texto e escrita
-        text_rect = text.get_rect(center=(LARGURA // 2, ALTURA // 2 - 10))  # posição do texto
+    pygame.display.update()  # Atualiza a tela para o jogador ver o novo frame
 
-        # ----- Gera saídas
 
-        screen.fill((0, 0, 0)) # Preenche com a cor preta no fundo
-        screen.blit(text, text_rect)  # coloca o texto na tela
-        pygame.display.flip()  # atualiza a tela
+    if event.type != pygame.QUIT:  # Verifica se o evento não é o de fechar a janela
+        Tela_Game_Over(LARGURA, ALTURA)  # Exibe a tela de game over
 
-        # Verificar se o tempo máximo foi atingido
-        tempo_atual = time.time()
-        tempo_decorrido = tempo_atual - tempo_inicial  # tempo decorrido desde o início do jogo
-        if tempo_decorrido >= tempo_maximo:
-            game = False
-
-            Tela_Game_Final = False
+    while estado == "PERDEU":
+        Tela_Game_Over(LARGURA, ALTURA)
+        # Resto do código para reiniciar o jogo ou sair do programa...
 
 # ===== Finalização =====
-pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
-
-"""
-queria dar um tempinho até que as coisas sejam lançadas de novo   
-
-as frutas não precisam ser lançadas todas ao mesmo tempo, pode ter um 
-intervalo de lançameto entre elas, teria como fazer isso?  
-
-
-fazer o efeito de corte das frutas
-colocar som no corte e no jogo
-
-
-"""
-
-##### Sons 
+pygame.quit()  # Encerra os recursos utilizados pelo Pygame
