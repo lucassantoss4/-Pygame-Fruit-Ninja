0bb2d67c631df8e35dcae8c935f05cbaaddaa818
Lucas Santos <lucass4@al.insper.edu.br>
2023-05-31 15:17:41 -0300

    modified:   0Lucas_copy.py

--- 0Lucas_copy.py
+++ 0Lucas_copy.py
@@ -155,104 +155,7 @@ lista_hits = [hit_0, hit_1, hit_2, hit_3, hit_4, hit_5, hit_6, hit_7, hit_8, hit
 # ----- Inicia estruturas de dados
 
 #-----------------Classes-----------------#
-
-class Fogo(pygame.sprite.Sprite):
-    # Construtor da classe.
-    def __init__(self, center, img):
-        # Construtor da classe mãe (Sprite).
-        pygame.sprite.Sprite.__init__(self)
-
-        # Armazena a animação de explosão
-        self.explosion_anim = lista_fogo
-
-        # Inicia o processo de animação colocando a primeira imagem na tela.
-        self.frame = 0  # Armazena o índice atual na animação
-        self.image = self.explosion_anim[self.frame]  # Pega a primeira imagem
-        self.rect = self.image.get_rect()
-        self.rect.center = center  # Posiciona o centro da imagem
-
-        # Guarda o tick da primeira imagem, ou seja, o momento em que a imagem foi mostrada
-        self.last_update = pygame.time.get_ticks()
-
-        # Controle de ticks de animação: troca de imagem a cada self.frame_ticks milissegundos.
-        # Quando pygame.time.get_ticks() - self.last_update > self.frame_ticks a
-        # próxima imagem da animação será mostrada
-        self.frame_ticks = 50
-
-    def update(self):
-        # Verifica o tick atual.
-        now = pygame.time.get_ticks()
-        # Verifica quantos ticks se passaram desde a ultima mudança de frame.
-        elapsed_ticks = now - self.last_update
-
-        # Se já está na hora de mudar de imagem...
-        if elapsed_ticks > self.frame_ticks:
-            # Marca o tick da nova imagem.
-            self.last_update = now
-
-            # Avança um quadro.
-            self.frame += 1
-
-            # Verifica se já chegou no final da animação.
-            if self.frame == len(self.explosion_anim):
-                # Se sim, tchau explosão!
-                self.kill()
-            else:
-                # Se ainda não chegou ao fim da explosão, troca de imagem.
-                center = self.rect.center
-                self.image = self.explosion_anim[self.frame]
-                self.rect = self.image.get_rect()
-                self.rect.center = center
-                
-class Fumaca(pygame.sprite.Sprite):
-    # Construtor da classe.
-    def __init__(self, center, img):
-        # Construtor da classe mãe (Sprite).
-        pygame.sprite.Sprite.__init__(self)
-
-        # Armazena a animação de explosão
-        self.explosion_anim = lista_fumaca
-
-        # Inicia o processo de animação colocando a primeira imagem na tela.
-        self.frame = 0  # Armazena o índice atual na animação
-        self.image = self.explosion_anim[self.frame]  # Pega a primeira imagem
-        self.rect = self.image.get_rect()
-        self.rect.center = center  # Posiciona o centro da imagem
-
-        # Guarda o tick da primeira imagem, ou seja, o momento em que a imagem foi mostrada
-        self.last_update = pygame.time.get_ticks()
-
-        # Controle de ticks de animação: troca de imagem a cada self.frame_ticks milissegundos.
-        # Quando pygame.time.get_ticks() - self.last_update > self.frame_ticks a
-        # próxima imagem da animação será mostrada
-        self.frame_ticks = 50
-
-    def update(self):
-        # Verifica o tick atual.
-        now = pygame.time.get_ticks()
-        # Verifica quantos ticks se passaram desde a ultima mudança de frame.
-        elapsed_ticks = now - self.last_update
-
-        # Se já está na hora de mudar de imagem...
-        if elapsed_ticks > self.frame_ticks:
-            # Marca o tick da nova imagem.
-            self.last_update = now
-
-            # Avança um quadro.
-            self.frame += 1
-
-            # Verifica se já chegou no final da animação.
-            if self.frame == len(self.explosion_anim):
-                # Se sim, tchau explosão!
-                self.kill()
-            else:
-                # Se ainda não chegou ao fim da explosão, troca de imagem.
-                center = self.rect.center
-                self.image = self.explosion_anim[self.frame]
-                self.rect = self.image.get_rect()
-                self.rect.center = center
-            
-
+ 
 #criando a classe de logos
 class Logos(pygame.sprite.Sprite):
     def __init__(self, img):
@@ -308,7 +211,7 @@ class Bombas(pygame.sprite.Sprite):
 # função para exibir a tela de game over
 def Tela_Game_Over(LARGURA, ALTURA):
     Tela_Game_Final = True # variável para o loop da tela de game over
-    tempo_maximo = 1  # tempo máximo para a tela de game over
+    tempo_maximo = 3  # tempo máximo para a tela de game over
     tempo_inicial = time.time()  # tempo inicial do jogo
 
     while Tela_Game_Final:
@@ -321,12 +224,15 @@ def Tela_Game_Over(LARGURA, ALTURA):
         pygame.display.set_caption('Game Over')  # nome da tela
         font = pygame.font.Font("util/fonte/upheavtt.ttf", 60)
         text = font.render('Game Over', True, (200, 0, 0))  # cor do texto e escrita
+        score = font.render('Score: ' + str(Score), True, (200, 0, 0))  # cor do texto e escrita
         text_rect = text.get_rect(center=(LARGURA // 2, ALTURA // 2 - 10))  # posição do texto
+        score_rect = score.get_rect(center=(LARGURA // 2, ALTURA // 2 + 50))  # posição do texto
 
         # ----- Gera saídas
 
         screen.fill((0, 0, 0))  # Preenche com a cor preta no fundo
         screen.blit(text, text_rect)  # coloca o texto na tela
+        screen.blit(score, score_rect)  # coloca o texto na tela
         pygame.display.flip()  # atualiza a tela
 
         # Verificar se o tempo máximo foi atingido
@@ -437,7 +343,6 @@ while game:
             Score += 100  # Incrementa a pontuação do jogador
             canal2.stop()
             canal2.play(random.choice(lista_hits))
-            # Fumaca(logo.rect.x, logo.rect.y)  # Cria um objeto "Fumaca" na posição do objeto "logo"
 
         if logo.rect.x > LARGURA or logo.rect.x - ALTURA_OBJ < 0:
             # Verifica se o objeto "logo" está fora dos limites da tela horizontalmente
